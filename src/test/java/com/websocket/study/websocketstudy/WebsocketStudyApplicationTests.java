2d4c4a2111634bd8f59800942de28b0645e256e9
lucasnscr <lucas.nascimento.scr@hotmail.com>
2019-02-22 12:12:05 -0300

    commit inicial

---
+++ src/test/java/com/websocket/study/websocketstudy/WebsocketStudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.websocket.study.websocketstudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebsocketStudyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
